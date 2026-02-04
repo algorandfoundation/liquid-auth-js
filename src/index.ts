3ab2bdd782e7fd24d4769f5ef4845c78faced0d2
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2026-02-04 10:27:11 +0300

    fix: barrel file expor

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,9 @@ export * as errors from './errors.js';
 
 // P2P Clients
 export * as signal from './signal.js';
+
+// Explicitly export SignalClient to facilitate importing in certain environments
+// that do not work well with subpath exports. Note that in those environments,
+// assertion and attestation flows will need to be implemented manually.
+export { SignalClient } from './signal.js';
+export { toBase64URL, fromBase64Url } from './encoding.js';
